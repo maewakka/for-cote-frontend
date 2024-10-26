a7ab6688121def8a1c897177e5f7d1c716b46882
woo <chlguddn7@gmail.com>
2024-10-26 14:30:41 +0900

    Footer 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,17 @@
 // src/components/Footer.js
 
 import React from 'react';
+import styles from '../styles/Footer.module.css';
 
 const Footer = () => {
   return (
-    <footer>
-      <p>&copy; 2024 Your Company. All rights reserved.</p>
+    <footer className={styles.footer}>
+      <p>
+        © 2023 All Rights Reserved.{' '}
+        <a href="https://startlink.io/" target="_blank" rel="noopener noreferrer" className={styles.link}>
+          주식회사 스타트링크
+        </a>
+      </p>
     </footer>
   );
 };
